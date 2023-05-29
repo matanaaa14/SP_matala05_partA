3ec25874c286b2fa2ab26fde4660260e5af268c2
matanaaa14 <117116975+matanaaa14@users.noreply.github.com>
2023-05-29 15:43:22 +0300

    Add files via upload

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -13,13 +13,49 @@ class MagicalContainer{
     void removeElement(int element);
     MagicalContainer();
     class AscendingIterator{
+        public:
         AscendingIterator();
         AscendingIterator(const MagicalContainer& other);
         ~AscendingIterator();
         AscendingIterator& operator=(const AscendingIterator& other);
         bool operator==(const AscendingIterator& other);
         bool operator!=(const AscendingIterator& other);
-
+        bool operator<(const AscendingIterator& other);
+        bool operator>(const AscendingIterator& other);
+        string operator*();
+        bool operator++();
+        AscendingIterator& begin();
+        AscendingIterator& end();
+    };
+    class PrimeIterator{
+        public:
+        PrimeIterator();
+        PrimeIterator(const MagicalContainer& other);
+        ~PrimeIterator();
+        PrimeIterator& operator=(const PrimeIterator& other);
+        bool operator==(const PrimeIterator& other);
+        bool operator!=(const PrimeIterator& other);
+        bool operator<(const PrimeIterator& other);
+        bool operator>(const PrimeIterator& other);
+        string operator*();
+        bool operator++();
+        PrimeIterator& begin();
+        PrimeIterator& end();
+    };
+    class SideCrossIterator{
+        public:
+        SideCrossIterator();
+        SideCrossIterator(const MagicalContainer& other);
+        ~SideCrossIterator();
+        SideCrossIterator& operator=(const SideCrossIterator& other);
+        bool operator==(const SideCrossIterator& other);
+        bool operator!=(const SideCrossIterator& other);
+        bool operator<(const SideCrossIterator& other);
+        bool operator>(const SideCrossIterator& other);
+        string operator*();
+        bool operator++();
+        SideCrossIterator& begin();
+        SideCrossIterator& end();
     };
 };
 #endif
